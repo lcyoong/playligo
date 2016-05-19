640f112db56468d94bbb80efbae9333a97012d94
LC <lcyoong@creino.com>
2016-05-19 13:30:42 +0800

    OG image change

--- resources/views/layouts/partials/meta.blade.php
+++ resources/views/layouts/partials/meta.blade.php
@@ -2,5 +2,5 @@
 <meta property="og:type"               content="website" />
 <meta property="og:title"              content="{{ $page_title or config('playligo.title') }}" />
 <meta property="og:description"        content="{{ $page_desc or config('playligo.desc') }}" />
-<meta property="og:image"              content="{{ $page_img or asset('img/logo.png') }}" />
-<meta property="fb:app_id"             content="{{ env('FACEBOOK_CLIENT_ID') }}" /> 
+<meta property="og:image"              content="{{ $page_img or asset('img/og-img.jpg') }}" />
+<meta property="fb:app_id"             content="{{ env('FACEBOOK_CLIENT_ID') }}" />
