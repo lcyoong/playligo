e2a193e7e48ee0c3050ae8d3e73c5e54872df105
LC <lcyoong@creino.com>
2016-06-17 20:16:26 +0200

    Add link to latest poll

--- resources/views/public/playlist_page.blade.php
+++ resources/views/public/playlist_page.blade.php
@@ -108,7 +108,7 @@
                 </div>
               </div>
               <div class="col-md-9 col-sm-9 col-xs-9">
-                {{ $poll->pol_title }}
+                <a href="{{ url('public_poll/' . $poll->pol_id) }}">{{ $poll->pol_title }}</a>
                 <div class="date_time">{{ Carbon::parse($poll->created_at)->diffForHumans() }}</div>
               </div>
             </div>
