9945d2a03c84652976c4b1fabe383587e65cc498
LC <lcyoong@creino.com>
2017-01-09 17:46:02 +0100

    Login redirection path using .env

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -33,7 +33,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = config('app.url');
     protected $redirectAfterLogout = '/';
 
     /**
